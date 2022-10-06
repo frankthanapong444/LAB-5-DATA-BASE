02f1520dd3e45aa15978c05b61202743564ee5f5
frang <franghero888@gmail.com>
2022-10-06 13:17:24 +0700

    Navigation & Route

--- Lab-5---Database-019fdf89b10a3ec4fcc445ae58cc4d67e932955b/lib/main.dart
+++ Lab-5---Database-019fdf89b10a3ec4fcc445ae58cc4d67e932955b/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'App Bunshe'),
     );
@@ -34,9 +34,18 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-        actions: [IconButton(icon: Icon(Icons.add), onPressed: () {})],
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Text("Screen 2");
+                }));
+              })
+        ],
       ),
       body: Container(),
     );
   }
 }
+//Navigation & Route
