9f33c677a6367422c3f2865c580c21d832daac83
frang <franghero888@gmail.com>
2022-10-06 13:20:04 +0700

    เพิ่มข้อมูล

--- Lab-5---Database-019fdf89b10a3ec4fcc445ae58cc4d67e932955b/lib/screens/form_screen.dart
+++ Lab-5---Database-019fdf89b10a3ec4fcc445ae58cc4d67e932955b/lib/screens/form_screen.dart
@@ -7,10 +7,32 @@ class FormScreen extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("หน้าที่ 2"),
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: Text("แบบฟอร์มบันทึกข้อมูล"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                  decoration: new InputDecoration(labelText: "ชื่อรายการ"),
+                  autofocus: true),
+              TextFormField(
+                decoration: new InputDecoration(labelText: "จำนวนเงิน"),
+                keyboardType: TextInputType.number,
+              ),
+              FlatButton(
+                color: Colors.purple,
+                textColor: Colors.white,
+                child: Text("เพิ่มข้อมูล"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          )),
+        ));
   }
 }
