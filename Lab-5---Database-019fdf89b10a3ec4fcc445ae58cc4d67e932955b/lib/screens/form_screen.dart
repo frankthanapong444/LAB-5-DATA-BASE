4dcb99ffeb855106f10a177818244ba8ba898ac2
frang <franghero888@gmail.com>
2022-10-06 13:18:52 +0700

    addPage

---
+++ Lab-5---Database-019fdf89b10a3ec4fcc445ae58cc4d67e932955b/lib/screens/form_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatelessWidget {
+  const FormScreen({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("หน้าที่ 2"),
+      ),
+      body: Container(),
+    );
+  }
+}
